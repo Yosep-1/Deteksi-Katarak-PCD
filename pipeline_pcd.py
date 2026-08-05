866dcd89b8afe707f8c2c191c69cc1c7634aa0aa
Dominic <dominic01tel@gmail.com>
2026-08-05 23:40:22 +0700

    Update pipeline_pcd.py

--- pipeline_pcd.py
+++ pipeline_pcd.py
@@ -1,9 +1,10 @@
-# Visual Pipeline (Citra Asli + Grayscale + Filtering + ROI/Segmentasi + Histogram + Edge Detection + Morfologi) 
+# -- FUNGSI FILE --
+# fungsi file untuk memberikan visual pipeline (Citra Asli + Grayscale + Filtering + ROI/Segmentasi + Histogram + Edge Detection + Morfologi) 
 
 import cv2
 import os
 
-# Folder Input (Data yang sudah di-augmentasi) dan Folder Hasil Pipeline
+# folder input (data yang sudah di augmentasi) dan folder hasil pipeline
 input_base = 'dataset_output'
 pipeline_base = 'dataset_pipeline'
 categories = ['Mata_Normal', 'Mata_Katarak']
@@ -18,7 +19,7 @@ for category in categories:
     
     print(f"Sedang memproses Visual Pipeline: {category}...")
     
-    # Kita proses semua 200 foto per kategori
+    # proses semua 200 foto per kategori
     for filename in os.listdir(path):
         img = cv2.imread(os.path.join(path, filename))
         if img is None: continue
@@ -26,13 +27,13 @@ for category in categories:
         # 1. Grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
-        # 2. Histogram Equalization (Kunci Akurasi!)
+        # 2. Histogram Equalization 
         equ = cv2.equalizeHist(gray)
         
-        # 3. Edge Detection (Canny)
+        # 3. Edge Detection 
         edges = cv2.Canny(equ, 100, 200)
         
-        # Simpan hasil untuk nanti dipajang di jurnal
+        # Simpan hasil 
         cv2.imwrite(os.path.join(save_path, f"gray_{filename}"), gray)
         cv2.imwrite(os.path.join(save_path, f"equ_{filename}"), equ)
         cv2.imwrite(os.path.join(save_path, f"edge_{filename}"), edges)
